ccd404aa4d89e4ffa1714ffa38cdecbaaa4a8d95
Jas <jas03x@gmail.com>
2023-09-16 20:17:42 -0400

    commit

--- Source/Gfx/Core/CHeapAllocator.cpp
+++ Source/Gfx/Core/CHeapAllocator.cpp
@@ -7,26 +7,11 @@
 #define IN_RANGE(x, a, b) (((x) >= (a)) && ((x) <= (b)))
 #define ALIGN(addr, alignment) (((addr) + ((alignment) - 1)) & ~((alignment) - 1))
 
-const uint64_t CHeapAllocator::PAGE_SIZES[CHeapAllocator::PAGE_SIZE__COUNT] =
-{
-	256, // PAGE_SIZE__256_BYTE
-	512, // PAGE_SIZE__512_BYTE
-	1 * KB, // PAGE_SIZE__1_KB
-	2 * KB, // PAGE_SIZE__2_KB
-	4 * KB, // PAGE_SIZE__4_KB
-	8 * KB, // PAGE_SIZE__8_KB 
-	16 * KB, // PAGE_SIZE__16_KB
-	32 * KB, // PAGE_SIZE__32_KB
-	64 * KB, // PAGE_SIZE__64_KB
-	128 * KB, // PAGE_SIZE__128_KB
-};
-
 CHeapAllocator::CHeapAllocator(void)
 {
-	ZeroMemory(&m_SortedLists, sizeof(m_SortedLists));
-	ZeroMemory(&m_ContiguousList, sizeof(m_ContiguousList));
-	ZeroMemory(&m_Chunks, sizeof(m_Chunks));
-	ZeroMemory(&m_PageEntries, sizeof(m_PageEntries));
+	m_PageEntryArray = nullptr;
+
+	ZeroMemory(&m_FreePages, sizeof(m_FreePages));
 }
 
 CHeapAllocator::~CHeapAllocator(void)
@@ -38,36 +23,32 @@ bool CHeapAllocator::Initialize(uint64_t HeapSizeInBytes)
 {
 	bool status = true;
 
-	if (HeapSizeInBytes % 2 != 0)
+	if (HeapSizeInBytes % PAGE_SIZE != 0)
 	{
 		status = false;
-		Console::Write(L"Error: Heap size not a power of 2\n");
+		Console::Write(L"Error: Heap size not divisible by the page size\n");
 	}
 
 	if (status)
 	{
-		// Initialize the heap with the biggest pages
-		PAGE_SIZE PageSize = static_cast<PAGE_SIZE>(PAGE_SIZE__COUNT - 1);
+		uint32_t NumPages = HeapSizeInBytes / PAGE_SIZE;
+		m_PageEntryArray = reinterpret_cast<PAGE_ENTRY*>(Memory::Allocate(NumPages * sizeof(PAGE_ENTRY), true));
 
-		for (uint64_t Offset = 0; Offset < HeapSizeInBytes; Offset += PAGE_SIZES[PageSize])
+		for (uint32_t i = 0; i < NumPages; i++)
 		{
-			PAGE_ENTRY* pEntry0 = AllocateEntry();
-			PAGE_ENTRY* pEntry1 = AllocateEntry();
-			if ((pEntry0 != nullptr) && (pEntry1 != nullptr))
+			PAGE_ENTRY* pEntry = &m_PageEntryArray[i];
+			pEntry->Offset = i * PAGE_SIZE;
+
+			if (m_FreePages.pHead == nullptr)
 			{
-				pEntry0->Size = PageSize;
-				pEntry0->Offset = Offset;
-
-				pEntry1->Size = PageSize;
-				pEntry1->Offset = Offset;
-
-				InsertTail(m_SortedLists[PageSize], pEntry0);
-				InsertTail(m_ContiguousList, pEntry1);
+				m_FreePages.pHead = pEntry;
+				m_FreePages.pTail = pEntry;
 			}
 			else
 			{
-				status = false;
-				break;
+				m_FreePages.pTail->pNext = pEntry;
+				pEntry->pPrev = m_FreePages.pTail;
+				m_FreePages.pTail = pEntry;
 			}
 		}
 	}
@@ -77,319 +58,109 @@ bool CHeapAllocator::Initialize(uint64_t HeapSizeInBytes)
 
 void CHeapAllocator::Uninitialize(void)
 {
-	PAGE_CHUNK* pChunk = m_Chunks.pHead;
-
-	while (pChunk != nullptr)
+	if (m_PageEntryArray != nullptr)
 	{
-		PAGE_CHUNK* pTemp = pChunk;
-
-		pChunk = pChunk->pNext;
-
-		Memory::Release(pTemp);
+		Memory::Release(m_PageEntryArray);
+		m_PageEntryArray = nullptr;
 	}
-}
-
-CHeapAllocator::PAGE_CHUNK* CHeapAllocator::AllocateChunk(void)
-{
-	uint8_t* pMemory = nullptr;
-	PAGE_CHUNK* pChunk = nullptr;
-	PAGE_ENTRY* pEntries = nullptr;
-
-	pMemory = reinterpret_cast<uint8_t*>(Memory::Allocate(sizeof(PAGE_CHUNK) + PAGE_ENTRIES_PER_CHUNK * sizeof(PAGE_ENTRY), true));
-
-	if (pMemory != nullptr)
-	{
-		pChunk = reinterpret_cast<PAGE_CHUNK*>(pMemory);
-		pEntries = reinterpret_cast<PAGE_ENTRY*>(pMemory + sizeof(PAGE_CHUNK));
-
-		if (m_Chunks.pHead == nullptr)
-		{
-			m_Chunks.pHead = pChunk;
-			m_Chunks.pTail = pChunk;
-		}
-		else
-		{
-			m_Chunks.pTail->pNext = pChunk;
-			m_Chunks.pTail = pChunk;
-		}
-
-		for (PAGE_ENTRY* pEntry = pEntries; pEntry != nullptr; pEntry = pEntry->pNext)
-		{
-			if (m_PageEntries.pHead == nullptr)
-			{
-				m_PageEntries.pHead = pEntry;
-				m_PageEntries.pTail = pEntry;
-			}
-			else
-			{
-				m_PageEntries.pTail->pNext = pEntry;
-				pEntry->pPrev = m_PageEntries.pTail;
-				m_PageEntries.pTail = pEntry;
-			}
-		}
-	}
-
-	return pChunk;
-}
-
-CHeapAllocator::PAGE_ENTRY* CHeapAllocator::AllocateEntry(void)
-{
-	bool status = true;
-	PAGE_ENTRY* pEntry = nullptr;
-
-	// Check if there are available entries ahead of time, if there aren't then allocate some
-	if (m_PageEntries.pHead == nullptr)
-	{
-		if (AllocateChunk() == nullptr)
-		{
-			status = false;
-		}
-	}
-
-	if (status)
-	{
-		pEntry = m_PageEntries.pHead;
-
-		if (pEntry == nullptr)
-		{
-			status = false;
-			Console::Write(L"Error: Could not allocate page entry\n");
-		}
-	}
-
-	if (status)
-	{
-		ZeroMemory(pEntry, sizeof(PAGE_ENTRY));
-
-		m_PageEntries.pHead = m_PageEntries.pHead->pNext;
-		m_PageEntries.pHead->pPrev = nullptr;
-		if (m_PageEntries.pHead == nullptr)
-		{
-			m_PageEntries.pTail = nullptr;
-		}
-	}
-
-	return pEntry;
-}
-
-CHeapAllocator::PAGE_ENTRY* CHeapAllocator::PopHead(PAGE_ENTRY_LINKED_LIST& List)
-{
-	PAGE_ENTRY* pEntry = nullptr;
-
-	if (List.pHead != nullptr)
-	{
-		pEntry = List.pHead;
-
-		List.pHead = List.pHead->pNext;
-
-		if (List.pHead != nullptr)
-		{
-			List.pHead->pPrev = nullptr;
-		}
-	}
-
-	return pEntry;
-}
-
-void CHeapAllocator::InsertTail(PAGE_ENTRY_LINKED_LIST& List, PAGE_ENTRY* pEntry)
-{
-	if (List.pHead == nullptr)
-	{
-		List.pHead = pEntry;
-		List.pTail = pEntry;
-	}
-	else
-	{
-		List.pTail->pNext = pEntry;
-		pEntry->pPrev = List.pTail;
-		List.pTail = pEntry;
-	}
-}
-
-CHeapAllocator::PAGE_ENTRY* CHeapAllocator::FindEntry(PAGE_ENTRY_LINKED_LIST& rList, uint64_t Offset, PAGE_SIZE Size)
-{
-	PAGE_ENTRY* pEntry = nullptr;
-
-	for (PAGE_ENTRY* pIt = rList.pHead; pIt != nullptr; pIt = pIt->pNext)
-	{
-		if ((pIt->Offset == Offset) && (pIt->Size == Size))
-		{
-			pEntry = pIt;
-			break;
-		}
-	}
-
-	return pEntry;
-}
-
-CHeapAllocator::PAGE_SIZE CHeapAllocator::GetPageSize(uint64_t Size)
-{
-	PAGE_SIZE PageSize = PAGE_SIZE__COUNT;
-
-	if (Size > PAGE_SIZES[PAGE_SIZE__COUNT - 1])
-	{
-		PageSize = static_cast<PAGE_SIZE>(PAGE_SIZE__COUNT - 1);
-	}
-	else
-	{
-		for (uint32_t i = PAGE_SIZE__256_BYTE; i < PAGE_SIZE__COUNT; i++)
-		{
-			if (Size <= PAGE_SIZES[i])
-			{
-				PageSize = static_cast<PAGE_SIZE>(i);
-				break;
-			}
-		}
-	}
-
-	return PageSize;
 }
 
 bool CHeapAllocator::Allocate(uint64_t Size, uint64_t Alignment, uint64_t& rOffset)
 {
 	bool status = true;
 
-	if (Size <= PAGE_SIZES[PAGE_SIZE__COUNT - 1])
+	if (Alignment % KB != 0)
 	{
-		status = AllocateOnePage(Size, Alignment, rOffset);
-	}
-	else
-	{
-		status = AllocateMultiplePages(Size, Alignment, rOffset);
+		status = false;
+		Console::Write(L"Error: Invalid alignment - not aligned to kilobytes\n");
 	}
 
-	return status;
-}
-
-bool CHeapAllocator::AllocateOnePage(uint64_t Size, uint64_t Alignment, uint64_t& rOffset)
-{
-	bool status = true;
-	PAGE_SIZE PageSize = GetPageSize(Size);
-	PAGE_ENTRY* pEntry = nullptr;
-
-	if (m_SortedLists[PageSize].pHead != nullptr)
+	if (((Alignment) & (Alignment - 1)) != 0)
 	{
-		pEntry = PopHead(m_SortedLists[PageSize]);
+		status = false;
+		Console::Write(L"Error: Invalid alignment - not a power of 2\n");
 	}
-	else
-	{
-		uint32_t n = PageSize + 1;
-		PAGE_ENTRY* pCandidatePage = nullptr;
 
-		// Start search for the next page size this allocation be placed inside
-		while (n < PAGE_SIZE__COUNT)
+	if (status)
+	{
+		uint32_t NumPages = ALIGN(Size, PAGE_SIZE) / PAGE_SIZE;
+
+		PAGE_ENTRY* pIt = m_FreePages.pHead;
+		while (pIt != nullptr)
 		{
-			for (PAGE_ENTRY* pPage = m_SortedLists[n].pHead; pPage != nullptr; pPage = pPage->pNext)
+			if ((pIt->Offset & (Alignment - 1)) == 0)
 			{
-				if (ALIGN(pPage->Offset, Alignment) <= pPage->Offset + PAGE_SIZES[n])
+				uint64_t NumFreePages = 1;
+				PAGE_ENTRY* pFirstPage = pIt; // The first page of this range
+
+				// Find consecutive free pages
+				while (NumFreePages < NumPages)
 				{
-					pCandidatePage = pPage;
-					break;
-				}
-			}
-
-			if (pCandidatePage != nullptr) { break; }
-			else                           { n++;   }
-		}
-
-		// If a valid page is found, break it down to get the desired page size
-		if (pCandidatePage != nullptr)
-		{
-			PAGE_ENTRY_LINKED_LIST NewPages[PAGE_SIZE__COUNT] = {};
-
-			// Find the contigous entry which will be replaced with the new pages
-			PAGE_ENTRY* pContigousEntry = FindEntry(m_ContiguousList, pCandidatePage->Offset, pCandidatePage->Size);
-
-			while (status && (n >= PageSize))
-			{
-				uint64_t SubPageOffset = pCandidatePage->Offset;
-
-				// Unlink the candidate
-				if (pCandidatePage->pPrev != nullptr)
-				{
-					pCandidatePage->pPrev->pNext = pCandidatePage->pNext;
-				}
-
-				if (pCandidatePage->pNext != nullptr)
-				{
-					pCandidatePage->pNext->pPrev = pCandidatePage->pPrev;
-				}
-
-				pCandidatePage->pPrev = nullptr;
-				pCandidatePage->pNext = nullptr;
-
-				ZeroMemory(pCandidatePage, sizeof(PAGE_ENTRY));
-				InsertTail(m_PageEntries, pCandidatePage);
-				pCandidatePage = nullptr;
-
-				// Break apart candidate page into subpages
-				uint64_t NumSubEntries = PAGE_SIZES[n] / PAGE_SIZES[n-1];
-
-				PAGE_ENTRY_LINKED_LIST& rSubPages = NewPages[n-1];
-
-				for (uint32_t i = 0; i < NumSubEntries; i++)
-				{
-					PAGE_ENTRY* pSubPage = AllocateEntry();
-
-					if (pSubPage == nullptr)
+					if ((pIt->pNext != nullptr) && (pIt->Offset + PAGE_SIZE == pIt->pNext->Offset))
 					{
-						status = false;
-						break;
+						NumFreePages++;
+						pIt = pIt->pNext;
 					}
-
-					pSubPage->Size = static_cast<PAGE_SIZE>(n-1);
-					pSubPage->Offset = SubPageOffset + i * PAGE_SIZES[n-1];
-
-					InsertTail(rSubPages, pSubPage);
-				}
-
-				// Find the next candidate page
-				for (PAGE_ENTRY* pPage = rSubPages.pHead; pPage != nullptr; pPage = pPage->pNext)
-				{
-					if (ALIGN(pPage->Offset, Alignment) <= pPage->Offset + PAGE_SIZES[n-1])
+					else
 					{
-						pCandidatePage = pPage;
 						break;
 					}
 				}
 
-				if (pCandidatePage == nullptr)
+				if (NumFreePages >= NumPages)
 				{
-					status = false;
-					Console::Write(L"Error: Could not find candidate page\n");
+					pIt = pFirstPage;
 					break;
 				}
-				
-				// Move onto next iteration
-				n--;
 			}
 
-			// TODO: INSERT ALL NEW PAGES INTO CURRENT FREE PAGES
+			pIt = pIt->pNext;
+		}
 
-			if (pCandidatePage != nullptr)
+		// If not null, points to enough pages to store the new allocation
+		if (pIt != nullptr)
+		{
+			ALLOCATION Allocation = {};
+			Allocation.Offset = pIt->Offset;
+			
+			for (uint32_t i = 0; i < NumPages; i++)
 			{
-				pEntry = pCandidatePage;
+				PAGE_ENTRY* pNext = pIt->pNext; //  Store next pointer which will be overwritten
+
+				// Unlink pIt
+				if (pIt->pPrev != nullptr) { pIt->pPrev->pNext = pIt->pNext; }
+				if (pIt->pNext != nullptr) { pIt->pNext->pPrev = pIt->pPrev; }
+
+				pIt->pNext = nullptr;
+				pIt->pPrev = nullptr;
+
+				// Insert pIt into the list
+				if (Allocation.Entries.pHead == nullptr)
+				{
+					Allocation.Entries.pHead = pIt;
+					Allocation.Entries.pTail = pIt;
+				}
+				else
+				{
+					Allocation.Entries.pTail->pNext = pIt;
+					pIt->pPrev = Allocation.Entries.pTail;
+					Allocation.Entries.pTail = pIt;
+				}
+
+				// Prepare for next iteration
+				pIt = pNext;
 			}
-			else
-			{
-				status = false;
-				Console::Write(L"Error: Failed to break down candidate page\n");
-			}
+
+			m_Allocations.push_back(Allocation);
+
+			rOffset = Allocation.Offset;
+		}
+		else
+		{
+			status = false;
+			Console::Write(L"Error: Could not find space to place the new allocation\n");
 		}
 	}
-
-	if (status && (pEntry != nullptr))
-	{
-		ALLOCATION Allocation = {};
-		Allocation.Offset = pEntry->Offset;
-	}
-
-	return status;
-}
-
-bool CHeapAllocator::AllocateMultiplePages(uint64_t Size, uint64_t Alignment, uint64_t& rOffset)
-{
-	bool status = true;
 
 	return status;
 }
--- Source/Gfx/Core/CHeapAllocator.hpp
+++ Source/Gfx/Core/CHeapAllocator.hpp
@@ -10,48 +10,20 @@ class CHeapAllocator
 private:
 	enum PAGE_SIZE : uint32_t
 	{
-		PAGE_SIZE__256_BYTE = 0,
-		PAGE_SIZE__512_BYTE = 1,
-		PAGE_SIZE__1_KB = 2,
-		PAGE_SIZE__2_KB = 3,
-		PAGE_SIZE__4_KB = 4,
-		PAGE_SIZE__8_KB = 5,
-		PAGE_SIZE__16_KB = 6,
-		PAGE_SIZE__32_KB = 7,
-		PAGE_SIZE__64_KB = 8,
-		PAGE_SIZE__128_KB = 9,
-		PAGE_SIZE__COUNT = 10
+		PAGE_SIZE = 1 * KB
 	};
-
-	static const uint64_t PAGE_SIZES[PAGE_SIZE__COUNT];
 
 	struct PAGE_ENTRY
 	{
-		PAGE_SIZE   Size;
 		uint64_t    Offset;
 		PAGE_ENTRY* pNext;
 		PAGE_ENTRY* pPrev;
 	};
 
-	struct PAGE_CHUNK
-	{
-		PAGE_ENTRY* pEntries;
-		PAGE_CHUNK* pNext;
-	};
-
-	// A chunk should take about 4KB
-	enum { PAGE_ENTRIES_PER_CHUNK = (4 * KB - sizeof(PAGE_CHUNK)) / sizeof(PAGE_ENTRY) };
-
 	struct PAGE_ENTRY_LINKED_LIST
 	{
 		PAGE_ENTRY* pHead;
 		PAGE_ENTRY* pTail;
-	};
-
-	struct PAGE_CHUNK_LINKED_LIST
-	{
-		PAGE_CHUNK* pHead;
-		PAGE_CHUNK* pTail;
 	};
 
 	struct ALLOCATION
@@ -62,11 +34,9 @@ private:
 
 	std::vector<ALLOCATION> m_Allocations;
 
-	PAGE_ENTRY_LINKED_LIST  m_SortedLists[PAGE_SIZE__COUNT]; // All pages available to be used (per block size)
-	PAGE_ENTRY_LINKED_LIST  m_ContiguousList; // All available pages stored contiguously
+	PAGE_ENTRY_LINKED_LIST  m_FreePages;
 
-	PAGE_CHUNK_LINKED_LIST  m_Chunks;      // All page chunks
-	PAGE_ENTRY_LINKED_LIST  m_PageEntries; // All available page entries
+	PAGE_ENTRY*             m_PageEntryArray;
 
 public:
 	CHeapAllocator(void);
@@ -76,19 +46,6 @@ public:
 	void        Uninitialize(void);
 
 	bool        Allocate(uint64_t Size, uint64_t Alignment, uint64_t& rOffset);
-
-private:
-	PAGE_CHUNK* AllocateChunk(void);
-	PAGE_ENTRY* AllocateEntry(void);
-
-	bool        AllocateOnePage(uint64_t Size, uint64_t Alignment, uint64_t& rOffset);
-	bool        AllocateMultiplePages(uint64_t Size, uint64_t Alignment, uint64_t& rOffset);
-
-	PAGE_ENTRY* PopHead(PAGE_ENTRY_LINKED_LIST& List);
-	void        InsertTail(PAGE_ENTRY_LINKED_LIST& List, PAGE_ENTRY* pEntry);
-	PAGE_ENTRY* FindEntry(PAGE_ENTRY_LINKED_LIST& rList, uint64_t Offset, PAGE_SIZE Size);
-
-	PAGE_SIZE   GetPageSize(uint64_t Size);
 };
 
 #endif // CG_HEAP_ALLOCATOR_HPP
